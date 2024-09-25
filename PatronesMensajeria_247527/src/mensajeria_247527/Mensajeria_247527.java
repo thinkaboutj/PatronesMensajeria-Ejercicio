366c119610dbc0a777c686440c0c257b8d26a62b
Jesus <159750556+thinkaboutj@users.noreply.github.com>
2024-09-24 23:38:37 -0700

    .....

--- PatronesMensajeria_247527/src/mensajeria_247527/Mensajeria_247527.java
+++ PatronesMensajeria_247527/src/mensajeria_247527/Mensajeria_247527.java
@@ -17,20 +17,20 @@ public class Mensajeria_247527 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      
-         Scanner tec = new Scanner(System.in);
+
+        Scanner tec = new Scanner(System.in);
         ManagerEntrega managerEntrega = new ManagerEntrega();
         String tipoPaquete, servicio;
         double distancia;
-        System.out.println("Bienvenido  mensajeria Express (╹ڡ╹ )");
-        System.out.println("Selecciona el tipo de paquete: (Sobre, CajaChica, CajaMediana, CajaGrande)");
+        System.out.println("Mensajeria Express (╹ڡ╹ )");
+        System.out.println("Elija el tipo de paquete: (Sobre, CajaChica, CajaMediana, CajaGrande)");
         tipoPaquete = tec.nextLine();
-        System.out.println("Selecciona el tipo de envio: (estandar, express)");
+        System.out.println("Elija el tipo de envio: (estandar, express)");
         servicio = tec.nextLine();
-        System.out.println("Favor de ingresar la distancia: ");
+        System.out.println("ingresa la distancia: ");
         distancia = tec.nextDouble();
-       
-        System.out.println("\n\n"+ managerEntrega.crearEntrega(servicio, distancia, tipoPaquete)+"\n\n");
+
+        System.out.println("\n\n" + managerEntrega.crearEntrega(servicio, distancia, tipoPaquete) + "\n\n");
     }
-    
+
 }
